0d1ef5df37c936b26ca2efe4b8d8149c970a645f
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-11-21 21:06:00 +0900

    COMMIT // minMaxWatt value

--- Please_Sunshine_API/routes/collection/company.js
+++ Please_Sunshine_API/routes/collection/company.js
@@ -7,60 +7,101 @@
 
 const express = require('express');
 const router = express.Router();
-const pool = require( '../../config/dbPool' ) ;	//	경로하나하나
-const async = require( 'async' ) ;
-const moment = require( 'moment' ) ;
+const pool = require('../../config/dbPool'); //	경로하나하나
+const async = require('async');
+const moment = require('moment');
 
-router.get( '/' , function( req , res ) {
+router.get('/', function(req, res) {
 
-	let task = [
+    let task = [
 
-		function( callback ) {
-			pool.getConnection(function(err , connection ) {
-				if(err) {
-					res.status(500).send({
-						status : "fail" ,
-						message : "internal server err"
-					});
-					callback( "getConnection err" );
-				} else {
-					callback( null , connection ) ;
-				}
-			});
-		} ,
+        function(callback) {
+            pool.getConnection(function(err, connection) {
+                if (err) {
+                    res.status(500).send({
+                        status: "fail",
+                        message: "internal server err"
+                    });
+                    callback("getConnection err");
+                } else {
+                    callback(null, connection);
+                }
+            });
+        },
 
-		function( connection , callback ) { 
+        function(connection, callback) {
 
-			let selectComapnyListQuery = 'SELECT * FROM Company C , PanelInfo PI WHERE C.c_id = PI.c_id ORDER BY if(ASCII(SUBSTRING(C.c_name , 1)) < 128, 9, 1) ASC , C.c_name ASC' ;
+            let selectComapnyListQuery = 'SELECT * FROM Company C , PanelInfo PI WHERE C.c_id = PI.c_id ORDER BY if(ASCII(SUBSTRING(C.c_name , 1)) < 128, 9, 1) ASC , C.c_name ASC';
 
-			connection.query( selectComapnyListQuery , function(err , result) {
-				if( err ) {
-					res.status(500).send({
-						status : "fail" ,
-						message : "internal server err"
-					}) ;
-					connection.release() ;
-					callback( "selectComapnyListQuery err") ;
-				} else {
+            connection.query(selectComapnyListQuery, function(err, result) {
+                if (err) {
+                    res.status(500).send({
+                        status: "fail",
+                        message: "internal server err"
+                    });
+                    connection.release();
+                    callback("selectComapnyListQuery err");
+                } else {
 
-					connection.release() ;
-					callback( null , result ) ;
-				}
-			}) ;
-		} ,
+                    let list = [];
 
-		function( list , callback ) {
+                    let index = result[0].c_id;
+                    let min = result[0].pi_watt;
+                    let max = result[0].pi_watt;
+                    for (let i = 0; i < result.length; i++) {
 
-			res.status(200).send({
-				status : "success" ,
-				data : list ,
-				message : "successful get CompanyList"
-			}) ;
-			callback( null , "successful get CompanyList" ) ;
-		}
-	] ;
+                        if (index == result[i].c_id) {
 
-	async.waterfall(task, function(err, result) {
+                            if (max < result[i].pi_watt)
+                                max = result[i].pi_watt;
+
+                            if (min > result[i].pi_watt)
+                                min = result[i].pi_watt;
+
+                        } else {
+
+                        	let minMaxWatt = min + "~" + max + "W" ;
+                        	let data = {
+                        		id : index ,
+                        		name : result[i-1].c_name ,
+                        		watt : minMaxWatt
+                        	}
+                        	list.push( data ) ;
+
+                        	index = result[i].c_id;
+                    		min = result[i].pi_watt;
+                    		max = result[i].pi_watt;
+                        }
+
+                        if( i == result.length - 1 ) {
+
+                        	let minMaxWatt = min + "~" + max + "W" ;
+                        	let data = {
+                        		id : index ,
+                        		name : result[i].c_name ,
+                        		watt : minMaxWatt
+                        	}
+                        	list.push( data ) ;
+                        }
+                    }
+                    connection.release();
+                    callback(null, list);
+                }
+            });
+        },
+
+        function(list, callback) {
+
+            res.status(200).send({
+                status: "success",
+                data: list,
+                message: "successful get CompanyList"
+            });
+            callback(null, "successful get CompanyList");
+        }
+    ];
+
+    async.waterfall(task, function(err, result) {
 
         let logtime = moment().format('MMMM Do YYYY, h:mm:ss a');
 
@@ -69,19 +110,6 @@ router.get( '/' , function( req , res ) {
         else
             console.log(' [ ' + logtime + ' ] ' + result);
     }); //async.waterfall
-}) ;
+});
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
